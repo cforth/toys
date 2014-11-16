300920bb832eee293f00e7339ceadb166a11d130
xiaohao@cforth <cforth@163.com>
2014-11-16 17:45:45 +0800

    二叉树查找算法，增加了删除节点的函数

--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -3,6 +3,8 @@
 import random
 
 class Node(object):
+    """使用Node类模二叉树的节点
+    """
     def __init__(self, key, value, number, left=None, right=None):
         self.key = key
         self.value = value
@@ -12,6 +14,8 @@ class Node(object):
 
 
 def get(key, node):
+    """查找二叉树中的key
+    """
     result = None
     while node != None:
         if key > node.key:
@@ -25,6 +29,8 @@ def get(key, node):
 
 
 def put(key, value, node):
+    """在二叉树中增加一个节点
+    """
     if node == None:
         return Node(key, value, 1)
     if key < node.key:
@@ -38,10 +44,135 @@ def put(key, value, node):
 
 
 def size(node):
+    """返回二叉树节点中的子节点（包含自己）数量
+    """
     if node == None:
         return 0
     else:
         return node.number
+
+
+def get_max(node):
+    """返回二叉树中所有节点中最大的key
+    """
+    while node.right != None:
+        node = node.right
+    max_item = node.key
+    return max_item
+
+
+def get_min(node):
+    """返回二叉树中所有节点中最小的key
+    """
+    while node.left != None:
+        node = node.left
+    min_item = node.key
+    return min_item
+
+
+def floor(key, node):
+    """返回二叉树所有节点的key中，小于等于key参数的最大的节点
+    """
+    if node == None:
+        return None
+    if key == node.key:
+        return node
+    if key < node.key:
+        return floor(key, node.left)
+    else:
+        right = floor(key, node.right)
+        if right == None:
+            return node
+        else:
+            return right
+
+
+def floor_key(key, node):
+    """返回二叉树所有节点的key中，小于等于key参数的最大的节点的key
+    """
+    floor_node = floor(key, node)
+    if floor_node != None:
+        return floor_node.key
+    else:
+        return None
+
+
+def ceiling(key, node):
+    """返回二叉树所有节点的key中，大于等于key参数的最小的节点
+    """
+    if node == None:
+        return None
+    if key == node.key:
+        return node
+    if key > node.key:
+        return ceiling(key, node.right)
+    else:
+        left = ceiling(key, node.left)
+        if left == None:
+            return node
+        else:
+            return left
+
+
+def ceiling_key(key, node):
+    """返回二叉树所有节点的key中，大于等于key参数的最小的节点的key
+    """
+    ceiling_node = ceiling(key, node)
+    if ceiling_node != None:
+        return ceiling_node.key
+    else:
+        return None
+
+
+def del_min(node):
+    """删除二叉树中所有节点key最小的节点
+    """
+    if node.left == None:
+        return node.right
+    node.left = del_min(node.left)
+    node.number = size(node.left) + size(node.right) + 1
+    return node
+
+
+def del_max(node):
+    """删除二叉树中所有节点key最大的节点
+    """
+    if node.rigth == None:
+        return node.left
+    node.rigth = del_max(node.rigth)
+    node.number = size(node.left) + size(node.right) + 1
+    return node
+
+
+def get_min_node(node):
+    """返回二叉树中最小key的节点
+    """
+    if node.left == None:
+        return node
+    else:
+        return get_min_node(node.left)
+
+
+def delete(key, node):
+    """删除二叉树中指定key的节点
+    """
+    if key > node.key:
+        node.right = delete(key, node.right)
+    elif key < node.key:
+        node.left = delete(key, node.left)
+    else:
+        if node.left == None:
+            return node.right
+        elif node.right == None:
+            return node.left
+        else:
+            t_left = node.left
+            t_right = node.right
+            node = get_min_node(t_right)
+            node.right = del_min(t_right)
+            node.left = t_left
+    node.number = size(node.left) + size(node.right) + 1
+    return node
 
 
 ## test
@@ -59,10 +190,9 @@ for i in range(25):
     num = random.randint(0, length-1)
     index = char_index[num]
     put(char_index[num], char_table[index], root)
-    print(char_index[num])
     char_index.pop(num)
     length -= 1
-print(root.number)
+print('All nodes number: %d' % root.number)
 
 ## 使用深度优先遍历和广度优先遍历二叉树
 def depth_first_search(root):
@@ -83,7 +213,7 @@ def breadth_first_search(root):
     quene.append(root)
     while len(quene) != 0 :
         node = quene[0]
-        print(node.key, end=' ')
+        print(node.value, end=' ')
         quene.pop(0)
         if node.left != None:
             quene.append(node.left)
@@ -91,5 +221,21 @@ def breadth_first_search(root):
             quene.append(node.right)
     print('\n')
 
+print('Depth first search:')
 depth_first_search(root)
+print('Breadth first search:')
 breadth_first_search(root)
+
+print('Max Key: %d' % get_max(root))
+print('Min Key: %d' % get_min(root))
+print('Floor key with 26: %d' % floor_key(26, root))
+print('Ceiling key with -1: %d' % ceiling_key(-1, root))
+
+print('Destroy Tree:')
+breadth_first_search(root)
+for i in range(26):
+    root = delete(i, root)
+    if root != None:
+        breadth_first_search(root)
+    else:
+        print('Tree have been destroyed！')
