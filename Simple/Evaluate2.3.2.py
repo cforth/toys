c0d258377eceeba76e60b3d56a3ab1168aba4d80
xiaohao@cforth <cforth@163.com>
2014-12-22 20:08:14 +0800

    大步操作语义实现

--- Simple/Evaluate2.3.2.py
+++ Simple/Evaluate2.3.2.py
@@ -59,6 +59,54 @@ class LessThan(object):
         return Boolean(self.left.evaluate(environment).value < self.right.evaluate(environment).value)
 
 
+class Assign(object):
+    def __init__(self, name, expression):
+        self.name = name
+        self.expression = expression
+
+    def evaluate(self, environment):
+        return dict(environment, **{self.name:self.expression.evaluate(environment)} )
+
+
+class DoNothing(object):
+    def evaluate(self, environment):
+        return environment
+
+
+class If(object):
+    def __init__(self, condition, consequence, alternative):
+        self.condition = condition
+        self.consequence = consequence
+        self.alternative = alternative
+
+    def evaluate(self, environment):
+        if self.condition.evaluate(environment).value == Boolean(True).value:
+            return self.consequence.evaluate(environment)
+        elif self.condition.evaluate(environment).value == Boolean(False).value:
+            return self.alternative.evaluate(environment)
+
+
+class Sequence(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def evaluate(self, environment):
+        return self.second.evaluate(self.first.evaluate(environment))
+
+
+class While(object):
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = body
+
+    def evaluate(self, environment):
+        if self.condition.evaluate(environment).value == Boolean(True).value:
+            return self.evaluate(self.body.evaluate(environment))
+        elif self.condition.evaluate(environment).value == Boolean(False).value:
+            return environment
+
+
 ##test
 print(Number(21).evaluate({}))
 
@@ -70,3 +118,20 @@ print(LessThan(
     Add(Variable('x'), Number(2)),
     Variable('y')
     ).evaluate({'x':Number(2), 'y':Number(5)}))
+
+
+statement = Sequence(
+    Assign('x', Add(Number(1), Number(1))),
+    Assign('y', Add(Variable('x'), Number(3)))
+    )
+print(statement.evaluate({}))
+print(dict([(k, v.value) for k,v in statement.evaluate({}).items()]))
+
+
+statement = While(
+    LessThan(Variable('x'), Number(5)),
+    Assign('x', Multiply(Variable('x'), Number(3)))
+    )
+
+print(statement.evaluate({'x': Number(1)}))
+print(dict([(k, v.value) for k,v in statement.evaluate({'x': Number(1)}).items()]))
