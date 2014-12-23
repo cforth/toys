85ac6d10071986d5bc0c9592b134383c7e63fe65
xiaohao@cforth <cforth@163.com>
2014-12-23 20:27:07 +0800

    指称语义：赋值语句实现

--- Simple/Denotation2.4.py
+++ Simple/Denotation2.4.py
@@ -52,19 +52,29 @@ class LessThan(object):
     def to_python(self):
         return 'eval(' + repr(self.left.to_python()) + ') < eval(' + repr(self.right.to_python()) + ')'    
 
+
+class Assign(object):
+    def __init__(self, name, expression):
+        self.name = name
+        self.expression = expression
+
+    def to_python(self):
+        return self.name + ' = ' + self.expression.to_python()
+        
+
 ##test
 
 ##数值表达式
 proc = Number(5).to_python()
 print(proc)
-num = eval(proc,{})
+num = eval(proc,{})      ##python的解释器中执行表达式的函数是exec()
 print(num, end = '\n\n')
 
 ##布尔值表达式
 proc = Boolean(True).to_python()
 print(proc)
 thing = eval(proc,{})
-print(thing, end = '\n\n')
+print(thing, end = '\n\n')                                                                                                       
 
 ##变量表达式
 proc = Variable('x').to_python()
@@ -74,7 +84,7 @@ print(var, end = '\n\n')
 
 ##环境
 environment = {'x':3}
-print('environment:%s' % repr(environment), end = '\n\n')
+print(environment, end = '\n\n')
 
 ##加法表达式
 proc = Add(Variable('x'), Number(1)).to_python()
@@ -93,3 +103,13 @@ proc = LessThan(Variable('x'), Add(Multiply(Variable('x'), Number(8)), Number(3)
 print(proc)
 result = eval(proc, environment)
 print(result, end = '\n\n')
+
+##赋值语句
+environment = {'x':3}
+proc = Assign('y', Number(5)).to_python()
+print(proc)
+exec(proc, environment)  ##python的解释器中执行语句的函数是exec()
+##print(environment, end = '\n\n')  ##经过exec()后的environment字典，为了方便演示，下面的语句不显示默认的环境变量键值对
+print(dict([(k, v) for k, v in environment.items() if '__' not in k]), end = '\n\n')
+
+
