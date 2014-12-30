ab81f5e8b36bb842d34a5eaddf8901bb55748d85
xiaohao@cforth <cforth@163.com>
2014-12-30 20:07:41 +0800

    非确定性有限自动机:nfa_design实现

--- Automaton/NFA3.2.py
+++ Automaton/NFA3.2.py
@@ -30,8 +30,42 @@ class NFARulebook(object):
     def rules_for(self, state, character):
         return [rule for rule in self.rules if rule.applies_to(state, character)]
 
-        
 
+class NFA(object):
+    def __init__(self, current_states, accept_states, rulebook):
+        self.current_states = current_states
+        self.accept_states = accept_states
+        self.rulebook = rulebook
+
+    def accepting(self):
+        if [state for state in self.current_states if state in self.accept_states]:
+            return True
+        else:
+            return False
+
+    def read_character(self, character):
+        self.current_states = self.rulebook.next_states(self.current_states, character)
+
+    def read_string(self, string):
+        for character in string:
+            self.read_character(character)
+
+
+class NFADesign(object):
+    def __init__(self, start_state, accept_states, rulebook):
+        self.start_state = start_state
+        self.accept_states = accept_states
+        self.rulebook = rulebook    
+
+    def to_nfa(self):
+        return NFA(set([self.start_state]), self.accept_states, self.rulebook)
+
+    def accepts(self, string):
+        nfa = self.to_nfa()
+        nfa.read_string(string)
+        return nfa.accepting()
+
+      
 ##test
 rulebook = NFARulebook([
     FARule(1, 'a', 1), FARule(1, 'b', 1), FARule(1, 'b', 2),
@@ -43,3 +77,8 @@ print(rulebook.next_states(set([1]), 'a'))
 print(rulebook.next_states(set([1, 2]), 'a'))
 print(rulebook.next_states(set([1, 3]), 'b'))
 
+nfa_design = NFADesign(1, [4], rulebook)
+
+print(nfa_design.accepts('bab'))
+print(nfa_design.accepts('bbbbbb'))
+print(nfa_design.accepts('bbabb'))
