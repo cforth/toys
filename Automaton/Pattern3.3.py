87a719a8461d08874edb673d9ea5cd0b44ccc197
xiaohao@cforth <cforth@163.com>
2015-01-07 21:53:02 +0800

    正则表达式:未完成

--- Automaton/Pattern3.3.py
+++ Automaton/Pattern3.3.py
@@ -2,6 +2,80 @@
 ##3.3 正则表达式
 ##python 3.4.1
 
+##NFA
+class FARule(object):
+    def __init__(self, state, character, next_state):
+        self.state = state
+        self.character = character
+        self.next_state = next_state
+
+    def applies_to(self, state, character):
+        return self.state == state and self.character == character
+
+    def follow(self):
+        return self.next_state
+
+
+class NFARulebook(object):
+    def __init__(self, rules):
+        self.rules = rules
+
+    def next_states(self, states, character):
+        n = []
+        for s in map(lambda state: self.follow_rules_for(state, character), states):
+            n += s
+        return set(n)
+
+    def follow_rules_for(self, state, character):
+        return [rule.follow() for rule in self.rules_for(state, character)]
+
+    def rules_for(self, state, character):
+        return [rule for rule in self.rules if rule.applies_to(state, character)]
+
+    def follow_free_moves(self, states):
+        more_states = self.next_states(states, None)
+        if more_states - states == set():
+            return states
+        else:
+            return self.follow_free_moves(states | more_states)
+
+
+class NFA(object):
+    def __init__(self, current_states, accept_states, rulebook):
+        self.current_states = rulebook.follow_free_moves(current_states)
+        self.accept_states = accept_states
+        self.rulebook = rulebook
+
+    def accepting(self):
+        if [state for state in self.current_states if state in self.accept_states]:
+            return True
+        else:
+            return False
+
+    def read_character(self, character):
+        self.current_states = self.rulebook.next_states(self.current_states, character)
+
+    def read_string(self, string):
+        for character in string:
+            self.read_character(character)
+
+
+class NFADesign(object):
+    def __init__(self, start_state, accept_states, rulebook):
+        self.start_state = start_state
+        self.accept_states = accept_states
+        self.rulebook = rulebook    
+
+    def to_nfa(self):
+        return NFA(set([self.start_state]), self.accept_states, self.rulebook)
+
+    def accepts(self, string):
+        nfa = self.to_nfa()
+        nfa.read_string(string)
+        return nfa.accepting()
+
+
+##正则表达式
 class Pattern(object):
     def bracket(self, outer_precedence):
         if self.precedence < outer_precedence:
@@ -12,6 +86,9 @@ class Pattern(object):
     def __str__(self):
         return '/' + self.to_s() + '/'
 
+    def matches(self, string):
+        return self.to_nfa_design().accepts(string)
+
 
 class Empty(Pattern):
     def __init__(self):
@@ -19,6 +96,12 @@ class Empty(Pattern):
         
     def to_s(self):
         return ''
+
+    def to_nfa_design(self):
+        start_state = object()
+        accept_states = [start_state]
+        rulebook = NFARulebook([])
+        return NFADesign(start_state, accept_states, rulebook)
 
 
 class Literal(Pattern):
@@ -29,6 +112,13 @@ class Literal(Pattern):
     def to_s(self):
         return self.character
 
+    def to_nfa_design(self):
+        start_state = object()
+        accept_state = object()
+        rule = FARule(start_state, self.character, accept_state)
+        rulebook = NFARulebook([rule])
+        return NFADesign(start_state, [accept_state], rulebook)
+
 
 class Concatenate(Pattern):
     def __init__(self, first, second):
@@ -38,6 +128,18 @@ class Concatenate(Pattern):
 
     def to_s(self):
         return ''.join([pattern.bracket(self.precedence) for pattern in [self.first, self.second]])
+
+    def to_nfa_design(self):
+        first_nfa_design = self.first.to_nfa_design()
+        second_nfa_design = self.second.to_nfa_design()
+
+        start_state = first_nfa_design.start_state
+        accept_states = second_nfa_design.accept_states
+        rules = first_nfa_design.rulebook.rules + second_nfa_design.rulebook.rules
+        extra_rules = [FARule(state, None, second_nfa_design.start_state) for state in first_nfa_design.accept_states]
+        rulebook = NFARulebook(rules + extra_rules)
+
+        return NFADesign(start_state, accept_states, rulebook)
 
 
 class Choose(Pattern):
@@ -69,3 +171,24 @@ pattern = Repeat(
             )
 
 print(pattern)
+
+print('')
+nfa_design = Empty().to_nfa_design()
+print(nfa_design.accepts(''))
+print(nfa_design.accepts('a'))
+nfa_design = Literal('a').to_nfa_design()
+print(nfa_design.accepts(''))
+print(nfa_design.accepts('a'))
+print(nfa_design.accepts('b'))
+
+print('')
+print(Empty().matches('a'))
+print(Literal('a').matches('a'))
+
+print('')
+pattern = Concatenate(Literal('a'), Literal('b'))
+print(pattern)
+print(pattern.matches('a'))
+print(pattern.matches('ab'))
+print(pattern.matches('abc'))
+
