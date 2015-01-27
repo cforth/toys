58aba5e39c34185037ff67cfaec6be5eb875d81d
Chai Fei <cforth@163.com>
2015-01-27 14:53:02 +0800

    DTM:TMRule

--- Turing/DTM.py
+++ Turing/DTM.py
@@ -39,11 +39,46 @@ class Tape(object):
         return Tape(left, middle, right, self.blank)
 
 
-## test
-tape = Tape(['1', '0', '1'], '1', [], '_')
-print(tape)
-print(tape.middle)
-print(tape.move_head_left)
-print(tape.write('0'))
-print(tape.move_head_right)
-print(tape.move_head_right.write('0'))
+class TMConfiguration(object):
+    def __init__(self, state, tape):
+        self.state = state
+        self.tape = tape
+
+
+class TMRule(object):
+    def __init__(self, state, character, next_state, write_character, direction):
+        self.state = state
+        self.character = character
+        self.next_state = next_state
+        self.write_character = write_character
+        self.direction = direction
+
+    def applies_to(self, configuration):
+        return self.state == configuration.state and self.character == configuration.tape.middle
+
+
+## UnitTest
+import unittest
+
+class TestDTM(unittest.TestCase):
+    """ Tests of the books's code
+    """
+        
+    def test_Tape(self):
+        tape = Tape(['1', '0', '1'], '1', [], '_')
+        self.assertEqual(str(tape), '#<Tape 101(1)>')
+        self.assertEqual(tape.middle, '1')
+        self.assertEqual(str(tape.move_head_left), '#<Tape 10(1)1>')
+        self.assertEqual(str(tape.write('0')), '#<Tape 101(0)>')
+        self.assertEqual(str(tape.move_head_right), '#<Tape 1011(_)>')
+        self.assertEqual(str(tape.move_head_right.write('0')), '#<Tape 1011(0)>')
+
+    def test_TMRule(self):
+        rule = TMRule(1, '0', 2, '1', 'right')
+        self.assertEqual(rule.applies_to(TMConfiguration(1, Tape([], '0', [], '_'))), True)
+        self.assertEqual(rule.applies_to(TMConfiguration(1, Tape([], '1', [], '_'))), False)
+        self.assertEqual(rule.applies_to(TMConfiguration(2, Tape([], '0', [], '_'))), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
